716c97e201f8147e80f5da996d3f5da18b20b704
Gavin <gavinchrisdsilva@gmail.com>
2018-09-26 03:01:36 +0530

    create a new bearer token for twitter api

---
+++ playground/get_bearer_token.js
@@ -0,0 +1,24 @@
+var request = require("request");
+
+var constants = require("../constants/constants");
+
+get_base64_Credentials = () => {
+    // var encodedUri = encodeURIComponent(constants.twitter_constants.consumer_key+":"+
+    // constants.twitter_constants.consumer_secret);
+    var constantsKey = constants.twitter_constants.consumer_key+":"+constants.twitter_constants.consumer_secret;
+    return Buffer.from(constantsKey).toString('base64');
+}
+
+console.log(get_base64_Credentials());
+
+request({
+    headers: {
+        "Authorization": "Basic "+get_base64_Credentials(),
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+    },
+    uri: constants.twitter_api.oauth_bearer_token,
+    body: "grant_type=client_credentials",
+    method: 'POST'
+  }, function (err, res, body) {
+    console.log(body, res.statusCode);
+  });
