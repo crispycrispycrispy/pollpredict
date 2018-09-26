aecffdc26b3a7b13de891bbd2116c6845e23f38e
Gavin <gavinchrisdsilva@gmail.com>
2018-09-26 19:43:03 +0530

    request for bearer token commented out

--- playground/get_bearer_token.js
+++ playground/get_bearer_token.js
@@ -11,14 +11,14 @@ get_base64_Credentials = () => {
 
 console.log(get_base64_Credentials());
 
-request({
-    headers: {
-        "Authorization": "Basic "+get_base64_Credentials(),
-        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
-    },
-    uri: constants.twitter_api.oauth_bearer_token,
-    body: "grant_type=client_credentials",
-    method: 'POST'
-  }, function (err, res, body) {
-    console.log(body, res.statusCode);
-  });
+// request({
+//     headers: {
+//         "Authorization": "Basic "+get_base64_Credentials(),
+//         "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"
+//     },
+//     uri: constants.twitter_api.oauth_bearer_token,
+//     body: "grant_type=client_credentials",
+//     method: 'POST'
+//   }, function (err, res, body) {
+//     console.log(body, res.statusCode);
+//   });
